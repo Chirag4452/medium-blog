0dd444a6f45eda59c0958edeb49cea742b9f5adb
Chirag4452 <chiragc77715@gmail.com>
2025-10-10 16:46:35 +0530

    Update dependencies in package.json and package-lock.json; remove blog and index routes; enhance user signup validation

--- backend/src/Routes/index.ts
+++ backend/src/Routes/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono'
-import { userRouter } from './user'
+import { userRouter } from '../user'
 import { blogRouter } from './blog'
 
 type Env = {
--- backend/src/user.ts
+++ backend/src/user.ts
@@ -2,21 +2,26 @@ import { Hono } from "hono";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 import { sign } from 'hono/jwt'
+import { signupInput } from './zod'
 
 type Env = {
   DATABASE_URL: string
   JWT_SECRET: string
 }
 
-
 export const userRouter = new Hono<{ Bindings: Env }>();
 
 userRouter.post('signup', async (c) => {
+	const body = await c.req.json();
+	const { success } = signupInput.safeParse(body);
+	if (!success) {
+		return c.json({
+			message: "Inputs not correct"
+		})
+	}
 	const prisma = new PrismaClient({
 		datasourceUrl: c.env.DATABASE_URL	,
 	}).$extends(withAccelerate());
-
-	const body = await c.req.json();
 	try {
 		const user = await prisma.user.create({
 			data: {
