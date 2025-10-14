c8fbeb39d5c4c16c0e40d45716f7547bd592d19c
Chirag4452 <chiragc77715@gmail.com>
2025-10-14 17:22:18 +0530

    Refactor user authentication logic and improve error handling in signup process

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,19 @@
+export const Quote = () => {
+    return( 
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center ">
+            <div className="max-w-lg">
+                <div className=" text-3xl font-bold">
+                    "The customer support I recived was Exceptional. The support team went above and beyond to address my concerns"
+                </div>
+                <div className="mt-4 max-w-md text-xl font-semibold">
+                    Julies Winfield
+                </div>
+                <div className="text-slate-500 max-w-md text-sm font-semibold font-light">
+                    CEO | Acme corp
+                </div>
+            </div>
+        </div>
+        
+    </div>
+)}
